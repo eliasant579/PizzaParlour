cf47cbd343b8782fcbccbd4fbc94951010a4fd98
eliasant579 <eliasant579@ed.amdsb.ca>
2018-10-05 14:20:47 -0700

    Still graphics' problem

--- PizzaParlour/Form1.Designer.cs
+++ PizzaParlour/Form1.Designer.cs
@@ -29,25 +29,49 @@
         private void InitializeComponent()
         {
             this.instructionsLabel = new System.Windows.Forms.Label();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // instructionsLabel
             // 
             this.instructionsLabel.AutoSize = true;
             this.instructionsLabel.ForeColor = System.Drawing.Color.White;
-            this.instructionsLabel.Location = new System.Drawing.Point(314, 209);
+            this.instructionsLabel.Location = new System.Drawing.Point(208, 199);
+            this.instructionsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.instructionsLabel.Name = "instructionsLabel";
-            this.instructionsLabel.Size = new System.Drawing.Size(51, 13);
+            this.instructionsLabel.Size = new System.Drawing.Size(193, 17);
             this.instructionsLabel.TabIndex = 0;
-            this.instructionsLabel.Text = "whatever";
+            this.instructionsLabel.Text = "Insert the number of toppings";
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(422, 199);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(100, 22);
+            this.inputTextbox.TabIndex = 1;
+            this.inputTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextbox_KeyDown);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(305, 265);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 17);
+            this.outputLabel.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputTextbox);
             this.Controls.Add(this.instructionsLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Shown += new System.EventHandler(this.Form1_Shown);
@@ -59,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- PizzaParlour/Form1.cs
+++ PizzaParlour/Form1.cs
@@ -12,6 +12,11 @@ namespace PizzaParlour
 {
     public partial class Form1 : Form
     {
+        //some useful variables
+        int toppings = 0;
+        const int STDPRICE = 7;
+        double price;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,9 +32,27 @@ namespace PizzaParlour
             Font writeFont = new Font("Arial", 16);
             SolidBrush writeBrush = new SolidBrush(Color.White);
 
-            g.DrawString("Pizza Parlour", writeFont, writeBrush, this.Width/2 - 80, 20);
+            g.DrawString("Pizza Parlour", writeFont, writeBrush, this.Width / 2 - 80, 20);
+        }
 
-
+        private void inputTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    toppings = Convert.ToInt16(inputTextbox.Text);
+                    price = STDPRICE + toppings * 1.25;
+                    outputLabel.Text = "You have to pay " + price.ToString("C");
+                }
+                catch
+                {
+                    outputLabel.Text = "Please, insert only numbers in the box above";
+                    inputTextbox.Text = null;
+                    return;
+                }
+            }
         }
     }
 }
+    
